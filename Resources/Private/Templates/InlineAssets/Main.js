d276d457cbd3238d4cd34b3daf88fddef2b3e811
Jon Uhlmann <me@jonnitto.ch>
2020-01-13 18:33:04 +0100

    :bug: Better iframe check

--- Resources/Private/Templates/InlineAssets/Main.js
+++ Resources/Private/Templates/InlineAssets/Main.js
@@ -1,10 +1,15 @@
 window.jonnittoIframeCheck = function(e) {
     var src = e.src;
     if (src) {
-        fetch(src).catch(function() {
-            console.error(`The iframe with the source ${src} returend an error, so the element was removed`);
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = src;
+        document.head.appendChild(script);
+        script.onerror = function() {
             var wrapperNode = e.closest('.jonnitto-iframe');
             wrapperNode.parentNode.removeChild(wrapperNode);
-        });
+        };
+        script.parentNode.removeChild(script);
     }
 };
